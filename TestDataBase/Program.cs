032e41cd7be0af13b01bbf58d825c5632cd64094
Vladislav <u197456@mail.ru>
2022-05-11 22:33:34 +0300

    DataBase train

---
+++ TestDataBase/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Base;
+
+
+namespace TestDataBase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataBase dataBase = new DataBase();
+            dataBase.initDbSettings();
+            dataBase.makeQuery("use productsdb;" +
+                                        "SELECT * FROM Products");
+        }
+    }
+}
---
+++ TestDataBase/WorkWithDataBase.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+
+namespace Base
+{
+	public class DataBase
+	{
+		string connetionString;
+		SqlConnection cnn;
+		public DataBase()
+		{
+
+
+		}
+		public void initDbSettings()
+		{
+
+			Console.WriteLine("Connecting... ");
+			connetionString = @"Data Source=(localdb)\MSSQLLocalDB;
+                            Initial Catalog=productsdb;";
+
+		}
+		public void makeQuery(string sqlExpression = "")
+		{
+			using (cnn = new SqlConnection(connetionString))
+			{
+				cnn.Open();
+				Console.WriteLine("Connection opened!");
+
+				using (SqlCommand command = new SqlCommand())
+				{
+					command.Connection = cnn;
+					command.CommandText = sqlExpression;
+
+
+					using (SqlDataReader reader = command.ExecuteReader())
+					{
+
+						if (reader.HasRows)
+							while (reader.Read())
+							{
+
+								Unit unit = new Unit();
+
+								int prodName = reader.GetOrdinal("ProductName");
+								string name = reader.GetString(prodName);
+								unit.name = name;
+								Console.WriteLine("Name:" + unit.name);
+								Console.WriteLine("------------\n");
+							}
+					}
+				}
+			}
+		}
+		public struct Unit
+		{
+			public string name { set; get; }
+
+			public int count { set; get; }
+			public string company { set; get; }
+			public int price { set; get; }
+		}
+	}
+}
